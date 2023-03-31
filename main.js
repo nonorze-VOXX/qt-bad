920dc23abd90d65d6c6b133e77d59edf6b43a9fd
nonorze-VOXX <appieosugood@gmail.com>
2023-03-31 15:09:57 +0800

    4 led can work

--- main.js
+++ main.js
@@ -13,35 +13,42 @@ app.get('/', function (req, res) {
 
     //post data 
     var add_data = req.body
-
-    // data = JSON.parse(data)
     console.log("1")
+    // data = JSON.parse(data)
     // res.end("")
+    // res.sendFile("/home/nvidia/code/lab4/lab4.html");
     res.sendFile("/home/lag/code/micro-computer/lab4/lab4.html");
 
 
 });
 let data = { "ON": "", "OFF": "" };
+let poutput = "";
+
 app.post('/led', function (req, res) {
     const form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
         if (err) {
         }
+        let onoff = '';
         data['ON'] = fields['ON'];
         data['OFF'] = fields['OFF'];
+        if (data['ON']) {
+            onoff = 'on';
+        } else if (data['OFF']) {
+            onoff = 'off';
+
+        }
         data['led1'] = fields['led1']
         data['led2'] = fields['led2']
         data['led3'] = fields['led3']
         data['led4'] = fields['led4']
-
-        if (fields['led4'] == 'on') {
-            exec("./onoff.out LED4 " + fields['led4'])
-        } else {
-            exec("./onoff.out LED4 off")
-
+        for (let i = 0; i < 4; i++) {
+            if (data['led' + i] == 'on') {
+                // exec("./onoff.out LED" + i + " " + fields['led' + i])
+                poutput += "./onoff.out LED" + i + " " + onoff + "<br>";
+            }
         }
-
-        res.end("i have " + JSON.stringify(data))
+        res.end("i have " + JSON.stringify(poutput))
     })
 });
 app.listen(8000)
