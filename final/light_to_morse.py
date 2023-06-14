031e70b5bddfabca2edad0fcdf4d094036958bfe
Your Name <you@example.com>
2023-06-14 23:09:17 +0800

    chiwawa

--- final/light_to_morse.py
+++ final/light_to_morse.py
@@ -138,11 +138,11 @@ def main():
             result = ""
             with open("./score") as file:
                 score=file.read()
-                if(int(score)>30):
+                if(int(score)>5):
                     mailtext="score: "+score
                     print(score)
                     send_email.sendMail(text+'\n'+mailtext)
-                    # exit(1)
+                    exit(1)
 
         if pre_light != is_light:  # light switch            
             # now_time = time.time()
--- final/send_email.py
+++ final/send_email.py
@@ -1,7 +1,10 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from pathlib import Path
 import smtplib
+import os
 
 from config import PASSWD
 def sendMail(text):
@@ -10,6 +13,14 @@ def sendMail(text):
     content["from"] = "ghostcheng.00@gmail.com"  #寄件者
     content["to"] = "t110590001@ntut.org.tw" #收件者
     content.attach(MIMEText("final text = "+text))  #郵件內容
+
+    os.system('rm *.jpg')
+    os.system('rm image.png')
+    os.system('nvgstcapture-1.0 --automate --capture-auto')
+    os.system('mv *.jpg image.jpg')
+    os.system('ffmpeg -i image.jpg image.png')
+    content.attach(MIMEImage(Path("image.png").read_bytes()))
+    content.attach(MIMEImage(Path("chiwawa.png").read_bytes()))
 
     with smtplib.SMTP(host="smtp.gmail.com", port="587") as smtp:  # 設定SMTP伺服器
         try:
@@ -20,3 +31,5 @@ def sendMail(text):
             print("Complete!")
         except Exception as e:
             print("Error message: ", e)
+
+sendMail('meme')
